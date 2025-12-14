a69bf0c4af18dbb1acf8243a66a34b7a7532ea36
Border Comfy <brendon@brendonchen.com>
2025-12-14 15:47:05 +0800

    Fix: Correct rotation nesting order

--- backend/scad_generator.py
+++ backend/scad_generator.py
@@ -53,8 +53,8 @@ module letter1_shape() {{
 }}
 
 module letter2_shape() {{
-    rotate([0, 0, 90])  // 第二步：繞 Z 軸轉 90 度
-        rotate([90, 0, 0])  // 第一步：繞 X 軸轉 90 度
+    rotate([90, 0, 0])  // 外層：對應 Three.js 的第一個 rotateX
+        rotate([0, 0, 90])  // 內層：對應 Three.js 的第二個 rotateZ
             linear_extrude(height=depth, center=true)
                 letter_geometry(letter2, font2, target_height);
 }}
